b3884e91a545eec8447eaf4dc12438c381f765b6
Jean-Yves Vet <contact@jean-yves.vet>
2022-11-10 23:44:16 +0100

    Splitting allocations and touching pages

--- oom-killer-trigger.c
+++ oom-killer-trigger.c
@@ -1,5 +1,5 @@
 /**
-* oom-killer-trigger: Allocate and map memory of given size to trigger an
+* oom-killer-trigger: Allocate and touch memory of given size to trigger an
 *                     Out-Of-Memory.
 *
 * URL       https://github.com/jyvet/oom-killer-trigger
@@ -14,12 +14,27 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MB          (1024 * 1024)
-#define CHUNK_MB_SZ  1024         /* 1 GB */
+#define MB                  (1024 * 1024)
+#define CHUNK_MB_SZ         128   /* 128 MB */
+#define WAIT_S              30    /* 30 seconds */
+#define PAGE_SZ             4096  /* 4 KB */
 
 /**
- * Allocate and initialize (to force mapping) 'size' MB of memory.
- */
+* Touch the firt byte of each page. It should be faster than initializing the
+* whole allocated chunk.
+*/
+void
+touch_pages(char *ptr, const uint64_t size)
+{
+    const uint64_t bytes = size * MB;
+
+    for (int i = 0; i <= bytes; i += PAGE_SZ)
+        ptr[i] = 1;
+}
+
+/**
+* Allocate 'size' MB of memory.
+*/
 void *
 allocate(const uint64_t size)
 {
@@ -29,41 +44,69 @@ allocate(const uint64_t size)
     void *ptr = malloc(size * MB);
 
     if (ptr == NULL)
-    {
-        printf("Allocation error. Exit...\n");
-        exit(1);
-    }
+        printf("Unable to allocate chunk of %luMB\n", size);
 
-    memset(ptr, 1, size * MB);
     return ptr;
 }
-
 
 int
 main (int argc, char *argv[])
 {
+    uint64_t i;
     if (argc != 2)
     {
-        printf("Allocate and initialize X MB.\nUsage %s <alloc_MB>\n", argv[0]);
+        printf("Allocate and map X MB.\nUsage %s <alloc_MB>\n", argv[0]);
         exit(0);
     }
 
     const uint64_t size_mbytes = atoi(argv[1]);
     printf("Allocating %lu MB...\n", size_mbytes);
 
-    const uint64_t size_chunk = size_mbytes / CHUNK_MB_SZ;
-    const uint64_t last_iter = size_mbytes % CHUNK_MB_SZ;
+    printf("Checking overcommit max size...\n");
 
-    for (uint64_t i = 0; i < size_chunk; i++)
+    const uint64_t chunks = size_mbytes / CHUNK_MB_SZ;
+
+    void **array_ptr = calloc(chunks, sizeof(void *));
+    if (array_ptr == NULL)
     {
-        printf("%lu MB... \t", i * CHUNK_MB_SZ);
-        void *ptr = allocate(CHUNK_MB_SZ);
-        printf(" DONE (%p)\n", ptr);
+        printf("Unable to allocate the array of pointers\n");
+        exit(1);
     }
 
-    allocate(last_iter);
 
-    sleep(10);
+    printf("Trying allocating %lu MB...\n", chunks * CHUNK_MB_SZ);
+    printf("Checking overcommit max size...\n");
+
+    for (i = 0; i < chunks; i++)
+    {
+        array_ptr[i] = allocate(CHUNK_MB_SZ);
+        if (array_ptr[i] == NULL)
+            break;
+    }
+
+    if (array_ptr[chunks - 1] != NULL)
+        printf("Total: allocated %lu MB\n", chunks * CHUNK_MB_SZ);
+    else
+        printf("Total: allocated %lu MB\n", i * CHUNK_MB_SZ);
+
+
+    printf("Wainting %ds...\n", WAIT_S);
+    sleep(WAIT_S);
+
+
+    printf("Touching pages... (it may generate an OOM)\n");
+
+    for (i = 0; i < chunks; i++)
+    {
+        if (array_ptr[i] == NULL)
+            break;
+
+        touch_pages((char*)array_ptr[i], CHUNK_MB_SZ);
+        printf("Touched %lu MB...\n", i + 1 * CHUNK_MB_SZ);
+    }
+
+    printf("Waiting again %ds...\n", WAIT_S);
+    sleep(WAIT_S);
 
     return 0;
 }
